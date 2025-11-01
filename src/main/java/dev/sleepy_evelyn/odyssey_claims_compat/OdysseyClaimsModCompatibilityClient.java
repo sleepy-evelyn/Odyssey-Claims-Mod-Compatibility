e52c87bbf1f69b0004f633c965eab5578d5182e3
sleepy-evelyn <46009144+sleepy-evelyn@users.noreply.github.com>
2025-11-01 22:23:56 +0000

    Initial Commit

---
+++ src/main/java/dev/sleepy_evelyn/odyssey_claims_compat/OdysseyClaimsModCompatibilityClient.java
@@ -0,0 +1,31 @@
+package dev.sleepy_evelyn.odyssey_claims_compat;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = OdysseyClaimsModCompatibility.MODID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = OdysseyClaimsModCompatibility.MODID, value = Dist.CLIENT)
+public class OdysseyClaimsModCompatibilityClient {
+    public OdysseyClaimsModCompatibilityClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        OdysseyClaimsModCompatibility.LOGGER.info("HELLO FROM CLIENT SETUP");
+        OdysseyClaimsModCompatibility.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+}
